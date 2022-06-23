8e32d46259cb3310a7858f2d6b6aedd93499f5e8
salabh-an <salabh.n@aot-technologies.com>
2022-06-23 20:08:14 +0530

    - #35 Design tuning to the Event Calendar tool  - added new column for project names  - now shows short forms of project and engagement, instead of full names  - added popup when clicking an engagement  - now shows engagement names on activity instead of project name  - refactored the code for Engagement calendar

--- viz-lib/src/visualizations/event-calendar/components/Month.tsx
+++ viz-lib/src/visualizations/event-calendar/components/Month.tsx
@@ -1,11 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import * as moment from "moment";
 import { extendMoment } from "moment-range";
 import * as _ from "lodash";
 
 const extendedMoment = extendMoment(moment);
 
-const Month = ({ month, events, hoveredEvent, setHoveredEvent, ...rest }: any) => {
+const Month = ({ month, events, hoveredEvent, setHoveredEvent, handleEventClick }: any) => {
   const start = month;
   const end = extendedMoment.default(month).endOf("month");
   const dateRange = extendedMoment.range(start, end);
@@ -14,7 +14,7 @@ const Month = ({ month, events, hoveredEvent, setHoveredEvent, ...rest }: any) =
   const firstDay = start.weekday();
   const startOffset = Array.apply(null, Array(firstDay));
   const endOffset = Array.apply(null, Array(42 - (firstDay + dates.length)));
-  const weekDates = [...startOffset, ...dates, ...endOffset];
+  const monthDates = [...startOffset, ...dates, ...endOffset];
 
   const prepareEventDates = (events: any[]) => {
     return events.map((e: any) => {
@@ -33,6 +33,93 @@ const Month = ({ month, events, hoveredEvent, setHoveredEvent, ...rest }: any) =
   const eventsData = _.groupBy(prepareEventDates(_.slice(_.orderBy(events, "start_date"))), "project");
 
   const numTasks = Object.keys(eventsData).length + 1;
+
+  const handleMouseOver = (event: any) => {
+    setHoveredEvent(event.engagement_id);
+  };
+
+  const handleMouseLeave = () => {
+    setHoveredEvent(null);
+  };
+
+  const getShortForm = (str: string) => {
+    const matches = str.match(/\b(\w)/g);
+    const acronym = matches?.join("").toUpperCase() || "";
+    return acronym;
+  };
+
+  const renderMonthDates = () => [
+    <span style={{ gridColumn: "8 / 15", textAlign: "center" }}></span>,
+    monthDates.map((date: any) => (
+      <span className={date ? "day" : "blank-day"} style={!date ? { gridRow: `span ${numTasks + 1}` } : {}}>
+        {date && date.format("D")}
+      </span>
+    )),
+  ];
+
+  const renderProjectNames = () =>
+    _.keys(eventsData).map((projectName: string) => (
+      <span
+        className="project-name"
+        key={projectName}
+        title={projectName}
+        style={{ gridColumn: "8 / 15", textAlign: "center" }}>
+        {getShortForm(projectName)}
+      </span>
+    ));
+
+  const renderEvents = (projectData: any, projectStart: any, projectName: string) =>
+    projectData.map((event: any, i: number) => {
+      const style = {
+        height: "100%",
+        backgroundColor: event.color,
+        gridColumn: `${1 + Math.abs(projectStart.start_date.date() - event.start_date.date())} / ${2 +
+          event.end_date.date() -
+          projectStart.start_date.date()}`,
+        gridRow: 1,
+        zIndex: 100 + event.start_date.date(),
+      };
+      return (
+        <div
+          key={event.engagement_id}
+          className={`month-event-item ${hoveredEvent === event.engagement_id ? "hovered" : ""}`}
+          style={style}
+          title={`${event.title} | ${event.work_type} | ${event.phase}`}
+          onMouseEnter={() => handleMouseOver(event)}
+          onMouseLeave={handleMouseLeave}
+          onClick={() => handleEventClick(event.engagement_id)}>
+          {getShortForm(event.title)}
+        </div>
+      );
+    });
+
+  const renderProjects = () =>
+    _.keys(eventsData).map((projectName: string) => {
+      const projectData = eventsData[`${projectName}`];
+      if (projectData) {
+        const projectStart = _.minBy(projectData, "start_date");
+        const projectEnd = _.maxBy(projectData, "end_date");
+        const projectEventDuration = 1 + (projectEnd.end_date.date() - projectStart.start_date.date());
+        const style = {
+          gridColumn: `${14 + firstDay + projectStart.start_date.date()} / ${15 +
+            firstDay +
+            projectEnd.end_date.date()}`,
+          display: "grid",
+          gridTemplateColumns: `repeat(${projectEventDuration} , var(--gridSize))`,
+          gridTemplateRows: "var(--gridSize)",
+          alignItems: "center",
+        };
+
+        return (
+          <>
+            <span style={style} key={projectName}>
+              {renderEvents(projectData, projectStart, projectName)}
+            </span>
+          </>
+        );
+      }
+    });
+
   const style = {
     gridRow: `span ${numTasks + 1}`,
     gridTemplateRows: `repeat(${numTasks}, var(--gridSize))`,
@@ -43,69 +130,11 @@ const Month = ({ month, events, hoveredEvent, setHoveredEvent, ...rest }: any) =
       <div className="month-name-column" style={style}>
         <span className="name">{month.format("MMMM")}</span>
       </div>
-
-      {weekDates.map((date: any) => (
-        <span className={date ? "day" : "day blank-day"} style={!date ? { gridRow: `span ${numTasks + 1}` } : {}}>
-          {date && date.format("D")}
-        </span>
-      ))}
-      {_.keys(eventsData).map((projectName: string) => {
-        const projectData = eventsData[`${projectName}`];
-        if (projectData) {
-          const projectStart = _.minBy(projectData, "start_date");
-          const projectEnd = _.maxBy(projectData, "end_date");
-          const projectEventDuration = 1 + (projectEnd.end_date.date() - projectStart.start_date.date());
-          // 8 + firstDay + projectEnd.end_date.date() - 7 + firstDay + projectStart.start_date.date()
-          return (
-            <span
-              style={{
-                gridColumn: `${7 + firstDay + projectStart.start_date.date()} / ${8 +
-                  firstDay +
-                  projectEnd.end_date.date()}`,
-                display: "grid",
-                gridTemplateColumns: `repeat(${projectEventDuration} , var(--gridSize))`,
-                gridTemplateRows: "var(--gridSize)",
-              }}
-              key={projectName}>
-              {projectData.map((event: any, i: number) => {
-                return (
-                  <div
-                    key={event.engagement_id}
-                    className={`month-event-item ${hoveredEvent === event.engagement_id ? "hovered" : ""}`}
-                    style={{
-                      height: "100%",
-                      backgroundColor: event.color,
-                      // TODO: Change to event start date + event duration
-                      gridColumn: `${1 + Math.abs(projectStart.start_date.date() - event.start_date.date())} /  ${2 +
-                        event.end_date.date() -
-                        projectStart.start_date.date()}`,
-                      gridRow: 1,
-                      zIndex: 100 - event.end_date.date() + event.start_date.date(),
-                    }}
-                    title={event.title}
-                    onMouseEnter={() => setHoveredEvent(event.engagement_id)}
-                    onMouseLeave={() => setHoveredEvent(null)}>
-                    {projectName}
-                  </div>
-                );
-              })}
-              <span
-                style={{
-                  gridColumn: "1 / -1",
-                  backgroundColor: "transparent",
-                  zIndex: 101,
-                  gridRow: 1,
-                  pointerEvents: "none",
-                  opacity: 0,
-                }}>
-                {projectName}
-              </span>
-            </span>
-          );
-        }
-      })}
+      {renderMonthDates()}
+      {renderProjectNames()}
+      {renderProjects()}
     </div>
   );
 };
 
-export default Month;
+export default React.memo(Month);
